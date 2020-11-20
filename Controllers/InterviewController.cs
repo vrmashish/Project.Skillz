df0cb1982c7f1c8bf49c062239e3233c6883fadb
Ashish Verma <vrm.ashish@outlook.com>
2020-11-20 11:32:39 +0530

    update

---
+++ Controllers/InterviewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Skillz.Models;
+
+namespace Project.Skillz.Controllers
+{
+    public class InterviewController : Controller
+    {
+        public static List<Interview> interviews = new List<Interview>();
+        // GET: Interview
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            
+            return View(interviews);
+        }
+        
+        public ActionResult Create()
+        {
+            Interview temp1 = new Interview();
+            return View(temp1);
+
+        }
+        [HttpPost]
+        public ActionResult Create(Interview interview)
+        {
+            if (ModelState.IsValid)
+            {
+                interview.id = interviews.Count() + 1;
+                interviews.Add(interview);
+                return RedirectToAction("Table");
+            }
+            return RedirectToAction("Table");
+        }
+    }
+}
---
+++ Models/Interview.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project.Skillz.Models
+{
+    public class Interview
+    {
+        [Required]
+        public bool selected { get; set; }
+        public int id { get; set; }
+        [Display(Name ="#")]
+        [Required]
+        public int serialNo { get; set; }
+        [Display(Name ="Interview Name")]
+        [Required]
+        public string InterviewName { get; set; }
+        [Display(Name ="Skill Level")]
+        [Required]
+        public string SkillLevel { get; set; }
+
+        public string AssessmentModes { get; set; }
+        public string Status { get; set; }
+        public string Questions { get; set; }
+    }
+}
